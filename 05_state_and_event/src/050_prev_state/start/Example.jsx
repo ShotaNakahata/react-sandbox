3b032e0c7ca267da21fd5a9a395777801e1aa1dc
中畠昇汰 <pandora.osk@gmail.com>
2024-10-17 01:24:26 +0800

    add usestate prev

--- 05_state_and_event/src/050_prev_state/start/Example.jsx
+++ 05_state_and_event/src/050_prev_state/start/Example.jsx
@@ -1,7 +1,30 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+    const [Count,setCount]= useState(0)
+    const CountUp = ()=>{
+        setCount(Count+1);      
+        setCount(prevstate=>{
+            return prevstate+1;
+        });
+    };
+    const CountDown = ()=>{
+        setCount(Count-1)
+        setCount(prevstate=>{
+            return prevstate - 1;
+        })
+    }
+    return(
+        <>
+        <p>現在のカウント{Count}</p>
+        <button
+        onClick={CountUp}
+        >+</button>
+
+        <button
+        onClick={CountDown}>-</button>
+        </>
+    )
 };
 
 export default Example;
