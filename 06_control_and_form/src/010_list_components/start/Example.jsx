c630b65acf4a22275580c402b31f9208bf1d0831
中畠昇汰 <pandora.osk@gmail.com>
2024-10-17 08:10:22 +0800

    add unique key

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -3,13 +3,13 @@ const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
   const animal = animals.map((animal)=>{
-    return(<li>hello{animal}</li>)
+    return(<li key={animal}>hello{animal}</li>)
   })
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        {animal}
+        {animal }
       </ul>
     </>
   );
