9a5ab2979f2f7f4ddc836b57da7d491e71c34756
中畠昇汰 <pandora.osk@gmail.com>
2024-10-17 08:49:36 +0800

    add filter practice

--- 06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ 06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 const animals = ["Dog", "Cat", "Rat"];
 
+
+// const animal= ()=>{
+//   const isMatch = animals.filter(animals.indexOf(fillterVal)!== -1)
+//   const animal=isMatch.map((animal)=>{
+//     return(<li key={animal}>{animal}</li>);
+//   })
+// }
+
 const Example = () => {
+    const [fillterVal,setFilterVal]=useState("")
+    const animalList=animals.filter(animal=>animal.indexOf(fillterVal)!==-1).map((animal)=>{
+  return(<li key={animal}>{animal}</li>)
+})
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input type="text" value={fillterVal} onChange={(e)=>setFilterVal(e.target.value)} />
       <ul>
-        {animals
+        {/* {animals
           .map((animal) => (
           <li>{animal}</li>
-        ))}
+        ))} */}
+        {animalList}
       </ul>
     </>
   );
