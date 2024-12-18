b96cc3d82ddf0dcd393988b323f249f7830c338b
中畠昇汰 <pandora.osk@gmail.com>
2024-12-18 17:08:25 +0800

    06_control_and_form

--- 06_control_and_form/src/080_input_textarea/start/Example.jsx
+++ 06_control_and_form/src/080_input_textarea/start/Example.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] = useState("");
+  const clear =()=>{setVal("")}
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      <label htmlFor="123">ラベル</label>
+      <div>
+        <input
+          type="text"
+          id="123"
+          value={val}
+          placeholder="こんにちは"
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <textarea  
+        id="345" 
+        placeholder="こんにちは" 
+        value={val} 
+        onChange={(e)=>setVal(e.target.value)}
+        ></textarea>
+      </div>
+      <h3>{val}</h3>
+      <button onClick={clear}>クリア</button>
+    </div>
   );
 };
 
