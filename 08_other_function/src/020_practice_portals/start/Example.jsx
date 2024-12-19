c670ead6a7a96197696e582500ddadda324b8210
中畠昇汰 <pandora.osk@gmail.com>
2024-12-19 14:45:39 +0800

    08_other_function_2

--- 08_other_function/src/020_practice_portals/start/Example.jsx
+++ 08_other_function/src/020_practice_portals/start/Example.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import Toast from "./components/Toast";
-
+import { createPortal } from "react-dom";
 const Example = () => {
   const [toastOpen, setToastOpen] = useState(false);
+  const ToastPortal = ({children})=>{
+    const target = document.querySelector(".container.start")
+    return createPortal(children,target)
+  }
 
   return (
     <div>
@@ -20,12 +24,15 @@ const Example = () => {
       >
         トーストを表示する
       </button>
+      <ToastPortal>
       {toastOpen && (
           <Toast
             visible={toastOpen}
             handleCloseClick={() => setToastOpen(false)}
           />
       )}
+      </ToastPortal>
+      
     </div>
   );
 };
