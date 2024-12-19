54ac472376181acd4709c87a237a5f876a5deba4
中畠昇汰 <pandora.osk@gmail.com>
2024-12-19 15:31:24 +0800

    08_other_function_4

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -5,7 +5,7 @@ const Case1 = () => {
   const inputRef = useRef();
 
   const forcusInput = () => {
-    inputRef.current.focus()
+    inputRef.current.focus();
   };
 
   return (
@@ -22,10 +22,33 @@ const Case1 = () => {
   );
 };
 
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef()
+  return (
+    <div>
+      <h3>ユースケース2</h3>
+      <video style={{ maxWidth: "50%" }} ref={videoRef}>
+        <source src="../../../public/sample.mp4"></source>
+      </video>
+      <button onClick={()=>{
+        if(playing){
+          videoRef.current.pause();
+        }else{
+          videoRef.current.play();
+        }
+        setPlaying((prev)=>!prev)
+      }}>
+        {playing ? "stop" : "play"}</button>
+    </div>
+  );
+};
+
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
