f016428ce5ef3726ac2ba89775ae9916345d70de
中畠昇汰 <pandora.osk@gmail.com>
2024-12-19 11:39:40 +0800

    07_styling_component_2

--- 07_styling_component/src/040_css_in_js/start/Example.jsx
+++ 07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
-
+import styled from "styled-components";
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
+  const Styledbutton = styled.button`
+    margin: auto;
+    border-radius: 9999px;
+    border: none;
+    display: block;
+    width: 120px;
+    height: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    background-color: ${(props)=>props.isSelected?"pink":""}; 
+  `
+
   return (
     <>
-      <button
-        className={`btn ${isSelected ? "selected" : ""}`}
-        onClick={clickHandler}
-      >
-        ボタン
-      </button>
+      <Styledbutton isSelected={isSelected} onClick={clickHandler}>button</Styledbutton>
       <div style={{ textAlign: "center" }}>
         {isSelected && "クリックされました。"}
       </div>
