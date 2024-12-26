f15fcbcc7550747653a79b314fe3bb191dd83200
中畠昇汰 <pandora.osk@gmail.com>
2024-12-26 17:47:00 +0800

    12_hooks_p2

--- 12_hooks_p2/src/010_useEffect_init/start/Example.jsx
+++ 12_hooks_p2/src/010_useEffect_init/start/Example.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Example = () => {
-  return <></>;
+  const [time, setTime] = useState(0);
+
+  useEffect(()=>{
+    window.setInterval(()=>{
+      // console.log("set Time")
+      setTime(prev=>prev+1)
+    },1000)
+  },[]);
+
+  return (
+    <>
+    <h1>
+      <time>{time}</time>
+      <span>秒経過</span>
+    </h1>
+    </>
+  );
 };
 
 export default Example;
