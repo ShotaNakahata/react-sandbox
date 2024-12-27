e4f5677422990a3adb6eea3e1f5b143a607844ec
中畠昇汰 <pandora.osk@gmail.com>
2024-12-28 03:20:56 +0800

    13_redux_1

--- 13_redux/start/src/010_redux_no_rtk/components/CounterButton.jsx
+++ 13_redux/start/src/010_redux_no_rtk/components/CounterButton.jsx
@@ -1,8 +1,11 @@
+import { useDispatch } from "react-redux";
 import { useCounterDispatch } from "../context/CounterContext";
 
 const CounterButton = ({calcType, step}) => {
+
+    const dispatch = useDispatch();
     
-    const dispatch = useCounterDispatch();
+    // const dispatch = useCounterDispatch();
     
     const clickHandler = () => {
         dispatch({ type: calcType, step });
