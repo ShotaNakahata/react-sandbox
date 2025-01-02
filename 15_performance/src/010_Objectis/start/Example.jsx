e6ea09eae6246c0ae3806f5e0b09b9744d38053d
中畠昇汰 <pandora.osk@gmail.com>
2025-01-03 06:48:06 +0800

    15_performance_1

--- 15_performance/src/010_Objectis/start/Example.jsx
+++ 15_performance/src/010_Objectis/start/Example.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const Example = () => {
+  console.log("render from practice")
+  const [state,setState]=useState(0)
   return (
     <>
-    <h3>再レンダリングが発生する条件</h3>
+    {/* <h3>再レンダリングが発生する条件</h3>
     <p>stateの値が変更された時</p>
-    <p>Object.isによって変更は検知される</p>
+    <p>Object.isによって変更は検知される</p> */}
+    <button onClick={()=>setState(1)}> button A</button>
+    <div><p>ボタンAクリック回数: {state}</p></div>
     </>
   );
 };
