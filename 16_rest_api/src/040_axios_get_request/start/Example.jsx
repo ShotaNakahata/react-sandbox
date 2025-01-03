164365f4ed3ee6810ae91f8eb2847c213baa8a24
中畠昇汰 <pandora.osk@gmail.com>
2025-01-04 01:30:56 +0800

    16_rest_api_2

--- 16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ 16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,5 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Example = () => {
-  return <></>;
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios("http://127.0.0.1:3003/todo");
+      // console.log("from practioce",response.data)
+      setUser(response.data)
+      
+    };
+    getUser();
+  }, []);
+  console.log("users",users)
+  
+  return (
+    <div>
+      {users.map((user)=>{
+        return(
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>age: {user.age}</p>
+            <p>hobby: {user.hobbies}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
 };
+
+// {
+//   "id": 1,
+//   "username": "hoge太郎",
+//   "age": 20,
+//   "hobbies": ["サッカー", "野球"],
+//   "premiumAccount": true
+// },
 
 export default Example;
