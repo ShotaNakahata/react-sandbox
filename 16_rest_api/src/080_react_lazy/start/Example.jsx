35d735394141270a6500427bf3801378e961b119
中畠昇汰 <pandora.osk@gmail.com>
2025-01-04 05:08:22 +0800

    16_rest_api_5

--- 16_rest_api/src/080_react_lazy/start/Example.jsx
+++ 16_rest_api/src/080_react_lazy/start/Example.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
-import ComponentA from "./components/ComponentA";
+import { useState, lazy, Suspense } from "react";
+// import ComponentA from "./components/ComponentA";
 
+const LazyCompA = lazy(() => import("./components/ComponentA"));
+const LazyCompB = lazy(() => import("./components/ComponentB"));
 const Example = () => {
-  const [compA, setCompA] = useState(false);
+  const [compA, setCompA] = useState(true);
 
   return (
     <>
       <button onClick={() => setCompA((prev) => !prev)}>ComponentA</button>
-      {compA && <ComponentA />}
+      <Suspense fallback={<h3>now Loading.....</h3>
+      }
+      >{compA ? <LazyCompA/>:<LazyCompB/>}</Suspense>
     </>
   );
 };
