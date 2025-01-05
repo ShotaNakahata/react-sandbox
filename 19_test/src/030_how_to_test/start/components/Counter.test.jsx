0df700106ca5c174cb82930a984cef6dc09b972d
中畠昇汰 <pandora.osk@gmail.com>
2025-01-06 06:19:21 +0800

    19_test_3

--- 19_test/src/030_how_to_test/start/components/Counter.test.jsx
+++ 19_test/src/030_how_to_test/start/components/Counter.test.jsx
@@ -1,6 +1,19 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
 /* POINT テストコードを書く際の注意点
 Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+test("「カウントアップ」押下で「カウント」が＋１", () => {
+  //arrange
+  render(<Counter originCount={0} />);
+  const btn = screen.getByRole("button", { name: "カウントアップ" });
+
+  //act
+  fireEvent.click(btn);
+
+  //assertion
+  const spanEl = screen.getByText("現在のカウント:1");
+  expect(spanEl).toBeInTheDocument();
+});
