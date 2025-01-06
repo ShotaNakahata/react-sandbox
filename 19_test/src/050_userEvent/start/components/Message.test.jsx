280e5ebb84941f4ed29692dd653c2c56875e3f1a
中畠昇汰 <pandora.osk@gmail.com>
2025-01-06 09:31:41 +0800

    19_test_6

--- 19_test/src/050_userEvent/start/components/Message.test.jsx
+++ 19_test/src/050_userEvent/start/components/Message.test.jsx
@@ -1,5 +1,38 @@
-// import { render, screen } from "@testing-library/react";
-// import Message from "./Message";
-// import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import Message from "./Message";
+import userEvent from "@testing-library/user-event";
 
-test('ダミーテスト:削除して記載してください。', () => {});
+describe("Message Comp test", () => {
+  describe("初期表示確認", () => {
+    test("入力された文字の表示確認", () => {
+      render(<Message />);
+      const h2El = screen.getByRole("heading",{name:"入力された文字:"});
+      // console.log(h2El)
+      expect(h2El).toBeInTheDocument();
+    });
+    
+  });
+  describe("画面操作正常性確認", () => {
+    test("入力値の変更 -> 送信ボタン押下",async () => {
+      const user = userEvent.setup()
+      render(<Message />);
+      const inputEl = screen.getByRole("textbox")
+      const btnEl = screen.getByRole("button");
+      const h2El = screen.getByRole("heading",{name:"入力された文字:"});
+
+      expect(btnEl).toBeDisabled();
+      expect(inputEl.value).toBe("");
+      expect(h2El).toBeInTheDocument();
+
+      await user.type(inputEl,"こんにちは");
+      expect(inputEl.value).toBe("こんにちは");
+      expect(btnEl).not.toBeDisabled();
+
+      await user.click(btnEl)
+      expect(inputEl.value).toBe("");
+      expect(h2El.textContent).toBe("入力された文字:こんにちは");
+    });
+    
+  });
+});
