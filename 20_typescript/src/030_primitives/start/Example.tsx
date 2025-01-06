904c64b1d42c4c8797ba9208a1d0b4084b1ca244
中畠昇汰 <pandora.osk@gmail.com>
2025-01-06 16:09:54 +0800

    20_typescript_1

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,15 @@
 const Example = () => {
-  // 練習用
+  const str:string = "Hello"
+  console.log(str)
+
+  const num :number=102;
+  console.log(num)
+  
+  let bignum:bigint = 103n
+  console.log(bignum)
+
+  const bool:boolean=true
+  console.log(bool)
 };
 
 export default Example;
