9eeecec6c61ddb96056d465fdee8b778a31a2db5
中畠昇汰 <pandora.osk@gmail.com>
2025-01-06 14:16:11 +0800

    19_test_9

--- 19_test/src/070_function_mock/start/components/GetUserData.test.jsx
+++ 19_test/src/070_function_mock/start/components/GetUserData.test.jsx
@@ -1,23 +1,73 @@
-test('ダミーテスト:削除して記載してください。', () => {});
+// // test('ダミーテスト:削除して記載してください。', () => {});
+import { render, screen } from "@testing-library/react";
+import GetUserData from "./GetUserData";
+import { ENDPOINT_URL } from "../Example";
+import axios from "axios";
+
+vi.mock("axios");
+
+describe("GetUserDataコンポーネントの動作確認", () => {
+//   test("外部データ取得中", () => {
+//     render(<GetUserData url={ENDPOINT_URL} />);
+//     const h1El = screen.getByRole("heading", { name: "通信中です！" });
+//     expect(h1El).toBeInTheDocument();
+//   });
+
+
+  test("外部データ取得後", async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        id: 2,
+        name: "コードマフィア",
+      },
+    });
+    render(<GetUserData url={ENDPOINT_URL} />);
+    const h2El = await screen.findByRole("heading", { name: "コードマフィア" });
+    expect(h2El).toBeInTheDocument();
+
+    const itemEls = await screen.findAllByRole("listitem");
+
+    expect(itemEls[0].textContent).toBe("ID: 2");
+    expect(itemEls[1].textContent).toBe("Name: コードマフィア");
+  });
+});
+// POINT 外部APIのモック化の手法について学ぼう
+
+
 // import { render, screen } from "@testing-library/react";
 // import GetUserData from "./GetUserData";
 // import { ENDPOINT_URL } from "../Example";
+// import axios from "axios";
 
-// describe('GetUserDataコンポーネントの動作確認', () => {
-//   test('外部データ取得中', () => {
+// vi.mock("axios");
+
+// describe("GetUserDataコンポーネントの動作確認", () => {
+//   test("外部データ取得中", async () => {
+//     axios.get.mockResolvedValue({
+//       data: {
+//         id: 2,
+//         name: "コードマフィア",
+//       },
+//     });
 //     render(<GetUserData url={ENDPOINT_URL} />);
-//     const h1El = screen.getByRole('heading', { name: '通信中です！'});
-//     expect(h1El).toBeInTheDocument();
+//     // const h1El = await screen.findByRole("heading", { name: "通信中です！" });
+//     // expect(h1El).toBeInTheDocument();
 //   });
-//   test('外部データ取得後', async () => {
+
+//   test("外部データ取得後", async () => {
+//     axios.get.mockResolvedValue({
+//       data: {
+//         id: 2,
+//         name: "コードマフィア",
+//       },
+//     });
 //     render(<GetUserData url={ENDPOINT_URL} />);
-//     const h2El = await screen.findByRole('heading', { name: 'プロフィール'});
+//     const h2El = await screen.findByRole("heading", { name: "プロフィール" });
 //     expect(h2El).toBeInTheDocument();
 
-//     const itemEls = await screen.findAllByRole('listitem');
+//     const itemEls = await screen.findAllByRole("listitem");
 
-//     expect(itemEls[0].textContent).toBe("ID: 1");
-//     expect(itemEls[1].textContent).toBe("Name: CodeMafia");
+//     expect(itemEls[0].textContent).toBe("ID: 2");
+//     expect(itemEls[1].textContent).toBe("Name: コードマフィア");
 //   });
-// })
-
+// });
