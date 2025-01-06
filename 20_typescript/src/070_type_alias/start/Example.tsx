b362e5d3701b744908507c8626bdaa1fe7821de3
中畠昇汰 <pandora.osk@gmail.com>
2025-01-07 05:45:37 +0800

    20_typescript_4

--- 20_typescript/src/070_type_alias/start/Example.tsx
+++ 20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,35 @@
 const Example = () => {
-  // 練習用
+  type User = {
+    name: string,
+    age: number
+  }
+
+  const shota: User = {
+    name: "shota",
+    age: 23
+  }
+
+  console.log(shota)
+
+  type UserName = string
+  type UserAge = number
+  type UserGender = "man"|"woman"|"other"
+
+  type UserProfile = {
+    name:UserName,
+    age:UserAge,
+    gender:UserGender
+  }
+
+  const usetProfile:UserProfile={
+    name:"shota",
+    age:15,
+    gender:"man"
+  }
+
+  console.log(usetProfile)
+
+
 };
 
 export default Example;
