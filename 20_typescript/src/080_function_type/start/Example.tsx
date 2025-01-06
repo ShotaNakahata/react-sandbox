4541bb336095abcd5a300d681ec158b96bc6aae1
中畠昇汰 <pandora.osk@gmail.com>
2025-01-07 05:56:45 +0800

    20_typescript_6

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -10,6 +10,19 @@ const Example = () => {
   }
 
   console.log(sum2(1, 4))
+
+  const sum3 = (x: number, y: number): number => {
+    return x + y
+  }
+
+  console.log(sum3(1, 9))
+
+  type Sum = (x: number, y: number) => number
+  const sum4: Sum = (x, y) => {
+    return x - y
+  }
+
+  console.log(sum4(10, 3))
 };
 
 export default Example;
