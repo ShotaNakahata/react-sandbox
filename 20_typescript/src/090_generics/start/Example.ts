4389f4dcf912db9145e38f357ebe803baf4a55e1
中畠昇汰 <pandora.osk@gmail.com>
2025-01-07 06:06:11 +0800

    20_typescript_7

--- 20_typescript/src/090_generics/start/Example.ts
+++ 20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,22 @@
 const Example = () => {
-  // 練習用
+  const repeatStr = (value: string, times: number): string[] => {
+    return new Array(times).fill(value);
+  }
+
+  const repeatNum = (value: number, times: number): number[] => {
+    return new Array(times).fill(value);
+  }
+
+  console.log(repeatStr("A", 3))
+  console.log(repeatNum(1, 3))
+
+
+  const repeat = <T>(value: T, times: number): T[] => {
+    return new Array(times).fill(value)
+  }
+
+  console.log(repeat<number>(8, 3))
+  console.log(repeat<string>("str", 4))
 };
 
 export default Example;
