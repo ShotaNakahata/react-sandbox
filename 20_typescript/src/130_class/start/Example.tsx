04ada4cfa07eee123e5265b6ccbce9ebdd7c1080
中畠昇汰 <pandora.osk@gmail.com>
2025-01-07 07:15:44 +0800

    20_typescript_10

--- 20_typescript/src/130_class/start/Example.tsx
+++ 20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,24 @@
+// POINT TypeScriptでのクラスの扱い方を学ぼう
+class User {
+  public name: string;
+  public age: number;
+  // private age: number;
+  constructor(name:string,age:number) {
+    this.name = name,
+    this.age = age
+  }
+}
+
+
 const Example = () => {
-  // 練習用
+  const user = { name: "Hanako", age: 21 };
+  const user2 = new User("hanako",22)
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user2.age}</p>
+    </div>
+  );
 };
 
 export default Example;
