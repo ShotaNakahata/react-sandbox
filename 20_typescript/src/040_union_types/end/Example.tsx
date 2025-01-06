6169d4eb70339e944470a3b1754b4aacd33cdf9b
中畠昇汰 <pandora.osk@gmail.com>
2025-01-06 16:16:42 +0800

    20_typescript_1

--- 20_typescript/src/040_union_types/end/Example.tsx
+++ 20_typescript/src/040_union_types/end/Example.tsx
@@ -2,24 +2,24 @@
 // POINT T | U というように、| を用いてユニオン型を表す
 
 const Example = () => {
-  let strOrNum: string | number = 'Hello';
-  strOrNum = 123;
-  console.log(strOrNum);
+  // let strOrNum: string | number = 'Hello';
+  // strOrNum = 123;
+  // console.log(strOrNum);
   
-  let strOrNumOrBool: string | number | boolean = 'Hello';
-  strOrNumOrBool = true;
-  console.log(strOrNumOrBool);
+  // let strOrNumOrBool: string | number | boolean = 'Hello';
+  // strOrNumOrBool = true;
+  // console.log(strOrNumOrBool);
 
-  let helloOrNumOrBool: 'Hello' | number | boolean = false;
+  // let helloOrNumOrBool: 'Hello' | number | boolean = false;
 
-  type HelloOrNum = 'Hello' | number;
-  const hello: HelloOrNum = 'Hello';
+  // type HelloOrNum = 'Hello' | number;
+  // const hello: HelloOrNum = 'Hello';
 
-  type DayOfWeek =
-    | 'Monday'
-    | 'Tuesday'
+  // type DayOfWeek =
+  //   | 'Monday'
+  //   | 'Tuesday'
 
-  const day: DayOfWeek = 'Monday';
+  // const day: DayOfWeek = 'Monday';
 };
 
 export default Example;
--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,13 @@
 const Example = () => {
-  // 練習用
+  let strOrNum: string | number = "hello"
+  console.log(strOrNum)
+
+  strOrNum = 123
+  console.log(strOrNum)
+
+  type HelloOrNum = "hello" | number;
+  const hello: HelloOrNum = "hello"
+  console.log(hello)
 };
 
 export default Example;
