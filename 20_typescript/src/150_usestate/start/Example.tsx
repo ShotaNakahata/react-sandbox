b734ccb70f5cc06bb4bac997e4a4d813a26d7f31
中畠昇汰 <pandora.osk@gmail.com>
2025-01-07 07:26:10 +0800

    20_typescript_11

--- 20_typescript/src/150_usestate/start/Example.tsx
+++ 20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+type Todo = {
+    id:number,
+    text:string
+}
+
 const Example = () => {
-  
+  const [todos,setTodos]= useState<Todo[]>([{id:0,text:"homework"}])
 };
 
 export default Example;
